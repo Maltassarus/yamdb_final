73838966e380091a59789d4dde16a08cfe29a3cf
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-04-29 17:47:06 +0300

    борьба со статикой

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,11 +116,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/staticfiles/'
+#STATIC_URL = '/staticfiles/'
+#
+#STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+#
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
