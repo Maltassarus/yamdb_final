76886e70667bc85c2ebd5d58c77faee2e3e4ea8e
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-04-29 16:40:29 +0300

    борьба со статикой

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
+STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
