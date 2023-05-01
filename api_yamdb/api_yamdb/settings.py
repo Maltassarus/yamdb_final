382659b90ab55c27d497fa50f07b3bc36db338d0
Андрей Борзенко <borzenkoandreys@yandex.ru>
2023-05-01 16:16:35 +0300

    small edits

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -120,9 +120,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
